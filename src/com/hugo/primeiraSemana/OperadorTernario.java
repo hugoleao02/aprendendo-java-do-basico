e438e221c6130d47daf87c0fdc5999130e1d4bbe
hugo <h.ferreira147@gmial.com>
2023-02-23 20:31:13 -0300

    Operadores

---
+++ src/com/hugo/primeiraSemana/OperadorTernario.java
@@ -0,0 +1,13 @@
+package com.hugo.primeiraSemana;
+
+public class OperadorTernario {
+    public static void main(String[] args) {
+        int idade = 20;
+        String status = idade >= 18 ? "adulto" : "menor"; // retorna adulto
+
+        int a = 10;
+        int b = 15;
+        int maior = (a > b) ? a : b; //retorna falso
+
+    }
+}
---
+++ src/com/hugo/primeiraSemana/OperadoreUnarios.java
@@ -0,0 +1,22 @@
+package com.hugo.primeiraSemana;
+
+public class OperadoreUnarios {
+    public static void main(String[] args) {
+        int x = 5;
+        x++; // agora x é igual a 6
+
+        int y = 10;
+        y--; // agora y é igual a 9
+
+
+        boolean estaChovendo = true;
+        boolean naoEstaChovendo = !estaChovendo; // agora naoEstaChovendo é igual a false
+
+        int z = 7;
+        int w = -z; // agora w é igual a -7
+
+
+
+    }
+
+}
